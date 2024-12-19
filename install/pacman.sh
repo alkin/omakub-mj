b09cd0d28cf74d86e905e6c28ddd29a6297197ec
Ricardo Faust <ricardocfaust@gmail.com>
2024-12-19 16:45:04 -0300

    Refactor pacman initialization: Moved pacman setup commands from pacman.sh to boot.sh and removed pacman.sh file. Updated pacman configuration for improved performance and set mirrors to Brazil.

--- install/pacman.sh
+++
@@ -1,7 +0,0 @@
-# Init pacman
-sudo sed -i -e 's/#Color/Color/g' /etc/pacman.conf
-sudo sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /etc/pacman.conf
-sudo sed -i -e 's/pkg.tar.xz/pkg.tar/g' /etc/makepkg.conf
-sudo sed -i -e 's/pkg.tar.xz/pkg.tar/g' /etc/makepkg.conf
-
-sudo pacman-mirrors --country Brazil
