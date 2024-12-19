8b527e635e55f7723c143785d9b3bee14cb80ce4
Ricardo Faust <ricardocfaust@gmail.com>
2024-12-19 17:29:09 -0300

    Fixes

---
+++ install/custom/git.sh
@@ -0,0 +1,3 @@
+git config --global user.email "$OMAKUB_FIRST_RUN_EMAIL"
+git config --global user.name "$OMAKUB_FIRST_RUN_NAME"
+git config --global pull.rebase false
--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -9,8 +9,4 @@ git config --global alias.ps push
 git config --global alias.ft fetch
 git config --global alias.d diff
 git config --global alias.a add
-# git config --global pull.rebase true
-
-git config --global user.email "$OMAKUB_FIRST_RUN_EMAIL"
-git config --global user.name "$OMAKUB_FIRST_RUN_NAME"
-git config --global pull.rebase false
+git config --global pull.rebase true
